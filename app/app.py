47f9a0d489308e3506af6acb9533a6942b576aac
Rajesh Sakhamuri <your_email_for_github@example.com>
2025-12-22 20:23:25 +0530

    Enable immutable image tagging

--- app/app.py
+++ app/app.py
@@ -4,6 +4,6 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello again from CI/CD - Final build!")
+        self.wfile.write(b"Hello again from CI/CD - now with image tagging")
 
 HTTPServer(("0.0.0.0", 8080), Handler).serve_forever()
