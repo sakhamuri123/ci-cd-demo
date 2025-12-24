e89373a7fa12ffc3232e89413241b1b52c9f38c8
Rajesh Sakhamuri <your_email_for_github@example.com>
2025-12-24 17:46:24 +0530

    Version 2 for rollback testing

--- app/app.py
+++ app/app.py
@@ -4,6 +4,6 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Deployed automatically to ECS via GitHub Actions")
+        self.wfile.write(b"VERSION 2 - THIS WILL BE ROLLED BACK")
 
 HTTPServer(("0.0.0.0", 8080), Handler).serve_forever()
