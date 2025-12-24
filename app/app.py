d01ded89d1d3b17a659438d6331230f3c5ffe147
Rajesh Sakhamuri <your_email_for_github@example.com>
2025-12-24 18:35:17 +0530

    pushing version 3

--- app/app.py
+++ app/app.py
@@ -4,6 +4,6 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"VERSION 2 - THIS WILL BE ROLLED BACK")
+        self.wfile.write(b"VERSION 3 - THIS WILL BE ROLLED BACK to VERSION 2")
 
 HTTPServer(("0.0.0.0", 8080), Handler).serve_forever()
